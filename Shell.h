cf07f534aa32ce67bf69574348814f9097bf311e
MegaChie <workspace.mega@gmail.com>
2022-12-08 22:37:59 +0200

    Update Shell.h

--- Shell.h
+++ Shell.h
@@ -12,5 +12,8 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#define bufFlush -1
+#define bufFlush -1 
+#define infoStart {NULL, NULL, NULL, 0, 0, 0, 0,
+ NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, 0, 0, 0}
+
 #endif
