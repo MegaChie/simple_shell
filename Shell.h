422a1d173dc9aca5a09f8b5c4e00d1acbb90edf4
MegaChie <workspace.mega@gmail.com>
2022-12-08 22:19:05 +0200

    Update Shell.h

---
+++ Shell.h
@@ -0,0 +1,14 @@
+#ifndef someShell
+#define someShell
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#endif someShell
