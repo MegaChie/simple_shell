bdd896587e7f9499f8c23f671d77d7e206c0799c
MegaChie <workspace.mega@gmail.com>
2022-12-08 21:48:29 +0200

    WIP

--- Main.c
+++ Main.c
@@ -1,1 +1,38 @@
+#include "Shell.h"
 
+int main (int ac, char **av)
+{
+	infoTable info[] = { infoStart };
+	int fileDire = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fileDire)
+		: "r" (fileDire));
+	if (ac == 2)
+	{
+		fileDire = open(av[1], O_RDONLY);
+		if (fileDire == -1)
+		{
+			if (errno == EACCES)
+			{
+				exit(126);
+			}
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readFileDire = fileDire;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
+}
